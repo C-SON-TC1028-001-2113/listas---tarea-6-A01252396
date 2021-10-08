f356680ab1b66d29901d8b717c14bd26a8a134fd
A01252396 <A01252396@ITESM.MX>
2021-10-08 02:29:03 +0000

    Tarea 6: Programas que utilizan matrices (listas de listas)

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,12 @@
-
-
 def main():
-   
-
-
-
+    r=int(input())
+    c=int(input())
+    m=r*c
+    Lista=[]
+    for i in range (m):
+        n=int(input())
+        if n<10:
+            Lista.append(n)
+    print(Lista)        
 if __name__=='__main__':
     main()
